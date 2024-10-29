2dcedd8094157a01ec98717e9ba3c30a2ac24391
Ferkku <late.pudas@gmail.com>
2024-10-29 17:49:28 +0200

    US3 - Test3

--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -33,3 +33,8 @@ class TestSpreadSheet(TestCase):
         ss = SpreadSheet()
         ss.set("A1", "=1")
         self.assertEqual(1, ss.evaluate("A1"))
+
+    def test_formula_evaluate_invalid_string(self):
+        ss = SpreadSheet()
+        ss.set("A1", "='Apple")
+        self.assertEqual("#Error", ss.evaluate("A1"))
